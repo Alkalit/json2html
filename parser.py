fe8562564cbf4b91675fca74585609d7a42aa129
Alexey Kalinin <lyoffa412@gmail.com>
2018-07-17 13:08:53 +0300

    Задача 1 - простейший парсер готов

--- parser.py
+++ parser.py
@@ -1,2 +1,23 @@
-def func():
-    return 0
+import json
+
+
+class Parser:
+
+    title_tmp = '<h1>{}</h1>'
+    body_tmp = '<p>{}</p>'
+
+    def parse(self, json_doc):
+
+        parsed = json.loads(json_doc)
+
+        html_tags = []
+
+        for obj in parsed:
+            title = obj['title']
+            html_tags.append(self.title_tmp.format(title))
+
+            body = obj['body']
+            html_tags.append(self.body_tmp.format(body))
+
+
+        return ''.join(html_tags)
