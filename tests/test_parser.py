ae4086a90872856e8823508a7cbe6ca81fca1dc0
Alexey Kalinin <lyoffa412@gmail.com>
2018-07-17 12:56:08 +0300

    Задача 1 - тест парсера

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,10 +1,28 @@
 import unittest
 
-from ..parser import func
+from ..parser import Parser
 
 
-class TestMe(unittest.TestCase):
+class TestParser(unittest.TestCase):
 
-    def test_me(self):
+    def test_markup(self):
 
-        self.assertEqual(func(), 1)
+        json_doc = """
+            [
+                {
+                    "title": "Title #1",
+                    "body": "Hello, World 1!"
+                },
+                {
+                    "title": "Title #2",
+                    "body": "Hello, World 2!"
+                }
+            ]
+        """
+
+        expected = "<h1>Title #1</h1><p>Hello, World 1!</p><h1>Title #2</h1><p>Hello, World 2!</p>"
+
+        json2html_parser = Parser()
+        parsed = json2html_parser.parse(json_doc)
+
+        self.assertEqual(expected, parsed)
