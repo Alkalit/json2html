b8a703e8871bd11d0c805c6d363dfec520acbe41
Alexey Kalinin <lyoffa412@gmail.com>
2018-07-17 17:34:12 +0300

    Задача 3 - реализация отдельной списковой ноды и кастомного парсера

--- parser.py
+++ parser.py
@@ -1,5 +1,52 @@
 import json
 
+# TODO ordered dict?
+
+
+class MyCustomDecoder(json.JSONDecoder):
+
+    """
+    Базовый класс декодера не предоставляет хука для типа списка, исправим это недоразуменее.
+
+    Честно украдено отсюда:
+    https://stackoverflow.com/questions/10885238/python-change-list-type-for-json-decoding
+    """
+
+    def __init__(self, list_type=list,  **kwargs):
+        super(MyCustomDecoder, self).__init__(**kwargs)
+
+        # Use the custom JSONArray
+        self.parse_array = self.JSONArray
+
+        # Use the python implemenation of the scanner
+        self.scan_once = json.scanner.py_make_scanner(self)
+        self.list_type=list_type
+
+    def JSONArray(self, s_and_end, scan_once, **kwargs):
+        values, end = json.decoder.JSONArray(s_and_end, scan_once, **kwargs)
+        return self.list_type(values), end
+
+
+class ListNode:
+
+    ul_tmp = '<ul>{content}</ul>'
+    li_tmp = '<li>{content}</li>'
+
+    def __init__(self, list_of_tags):
+
+        self.list_of_tags = list_of_tags
+
+    def render(self):
+
+        li_list = []
+
+        for node in self.list_of_tags:
+            li_item = node.render()
+            li_item = self.li_tmp.format(content=li_item)
+            li_list.append(li_item)
+
+        return self.ul_tmp.format(content=''.join(li_list))
+
 
 class Node:
 
@@ -22,26 +69,17 @@ class Node:
 
 class Parser:
 
-    ul_tmp = '<ul>{content}</ul>'
-    li_tmp = '<li>{content}</li>'
-
     def parse(self, json_doc):
 
+        parsed = json.loads(json_doc, cls=MyCustomDecoder, object_hook=self._object_hook, list_type=ListNode)
+
         # import ipdb; ipdb.set_trace()
-        parsed = json.loads(json_doc, object_hook=self._object_hook)
-
-        li_list = []
-
-        for item in parsed:
-            li_item = self.li_tmp.format(content=item)
-            li_list.append(li_item)
-
-        return self.ul_tmp.format(content=''.join(li_list))
+        return parsed.render()
 
     def _object_hook(self, obj):
 
         node = Node(obj)
-        return node.render() # NOTE лучше возвращать саму ноду и потом вручную отрендерить в списке. При этом можно делать местные проверки.
+        return node#.render() # NOTE лучше возвращать саму ноду и потом вручную отрендерить в списке. При этом можно делать местные проверки.
 
 
 def main():
